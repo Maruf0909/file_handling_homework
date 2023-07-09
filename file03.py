9a45581f4c43d1c8e193c7eec62fc9612a6c02c4
Mac <mac@MacBook-Air-Mac.local>
2023-07-09 20:28:46 +0500

    file3

--- file03.py
+++ file03.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    list=[]
+    i=0
+    for i in data:
+        if i>='0' and i<='9':
+            list.append(i)
+    return list
+
 
 # Read data from file
+f = open("data/data03.txt",encoding='utf8')
+data = f.read()
+f.close
+print(main(data))
