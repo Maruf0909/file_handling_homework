736507a68c4f4b8a413fcbbbc5db6ba101bf4635
Mac <mac@MacBook-Air-Mac.local>
2023-07-09 19:53:59 +0500

    file1

--- file01.py
+++ file01.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    
+    #x=[]
+    #for i in data:
+        #x.append (list(i))
+    data=data.split( )
+    return  (list(data))
 # Read data from file
+f = open("data/data01.txt", "r")
+data = f.read()
+f.close
+print(main(data))
