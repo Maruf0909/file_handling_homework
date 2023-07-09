0652b2e173bcb062a122b3a1dac9c3f85118aa90
Mac <mac@MacBook-Air-Mac.local>
2023-07-09 20:55:23 +0500

    file4

--- file04.py
+++ file04.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    x=[]
+    i=0
+    num=['0','1','2','3','4','5','6','7','8','9']
+    for i in data:
+        if i not in num:
+            x.append(i)
+    return x
     
 # Read data from file
+f=open("data/data04.txt","r")
+data=f.read()
+f.close
+print(main(data))
